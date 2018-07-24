c090b0c188ddaa3cb24c5dd2f9a57ac94dc7ed5f
Semyon Vyskubov <sv@rv7.ru>
2018-07-24 20:03:43 +0200

    Done #3: Remote settings overrides

--- options.js
+++ options.js
@@ -1,18 +1,25 @@
-var enableOnStartElement = document.getElementById("enableOnStartElement");
-// Saves options to localStorage.
-function save_options() {
-    var newValue = "off";
-    if (enableOnStartElement.checked) {
+const save = document.getElementById( "save" );
+const enableOnStartElement = document.getElementById( "enableOnStartElement" );
+const defaultSettingsUrlElement = document.getElementById( "defaultSettingsUrlElement" );
+
+function saveOptions () {
+    console.log( '!' );
+    let newValue = "off";
+    if ( enableOnStartElement.checked ) {
         newValue = "on";
     }
     localStorage["turnOnByDefault"] = newValue;
-}
-// Restores select box state to saved value from localStorage.
-function restore_options() {
-    if (localStorage["turnOnByDefault"] === "on") {
-        enableOnStartElement.checked = true;
-    }
+    localStorage["defaultSettingsUrl"] = defaultSettingsUrlElement.value.trim();
 }
 
-enableOnStartElement.addEventListener("change", save_options);
-restore_options();
+// Restores select box state to saved value from localStorage.
+function restoreOptions () {
+    if ( localStorage["turnOnByDefault"] === "on" ) {
+        enableOnStartElement.checked = true;
+    }
+
+    defaultSettingsUrlElement.value = localStorage["defaultSettingsUrl"];
+}
+
+save.addEventListener( "click", saveOptions );
+restoreOptions();
